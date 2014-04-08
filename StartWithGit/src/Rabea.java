952fc3b5632fce5acc56a9fa06fc7d1daba3e26e
khilo <khilooo_15@hotmail.com>
2014-04-09 00:14:40 +0300

    Commit Them Push

--- StartWithGit/src/Rabea.java
+++ StartWithGit/src/Rabea.java
@@ -4,6 +4,7 @@ public class Rabea {
 	public static void main(String[] a){
 		System.out.println("GIT GIT GIT");
 		System.out.println("Commet it Bitch");
-		System.out.println("10B$");
+		System.out.println("10B$");	
+		System.out.println("Workss");
 	}
 }
